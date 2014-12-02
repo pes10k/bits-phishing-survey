4402a958136ad01053ac207fe2e81d9d1868c2b9
Peter Snyder <snyderp@gmail.com>
2014-12-02 14:15:39 +0000

    updates to mail script

--- scripts/mail.py
+++ scripts/mail.py
@@ -4,6 +4,7 @@ import smtplib
 import sys
 from hashlib import sha1
 
+from subprocess import Popen, PIPE
 from email.utils import formatdate, make_msgid
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -43,7 +44,7 @@ msg['Message-ID'] = make_msgid()
 text = """\
 Dear UIC Security Study Participant,
 
-Thank you very much for your participation in the UIC Security Study.  The study period is now over.  If you have met the participation requirements during the study period, you show receive the second $15 Amazon Credit as compensation for your participation in the study.
+Thank you very much for your participation in the UIC Security Study.  The study period is now over.  If you have met the participation requirements during the study period, you should receive the second $15 Amazon Credit as compensation for your participation in the study.
 
 Please fill out this brief survey, to help us better understand any problems you may have encountered during the study.  The survey should take 5 minutes or less.
 
@@ -71,7 +72,7 @@ html = """\
     <body>
         <p>Dear UIC Security Study Participant,</p>
 
-        <p>Thank you very much for your participation in the UIC Security Study.  The study period is now over.  If you have met the participation requirements during the study period, you show receive the second $15 Amazon Credit as compensation for your participation in the study.</p>
+        <p>Thank you very much for your participation in the UIC Security Study.  The study period is now over.  If you have met the participation requirements during the study period, you should receive the second $15 Amazon Credit as compensation for your participation in the study.</p>
 
         <p>Please fill out <a href="https://uic-auth.com/bluestem/login.cgi?token={0}&cacheid=1417466719-97737178;return=68747470733a2f2f7777772e63732e7569632e6564752f;msg=;prior=1800;RetrieveURL=2f626c75657374656d2f6367692f72657472696576655f617574682e636769;BSVersion=1.6;BSVersionHash=b150eb45e429d51811b25dc2f79e11a112243b07;setcookie=1">this brief survey</a>, to help us better understand any problems you may have encountered during the study.  The survey should take 5 minutes or less.</p>
 
@@ -98,9 +99,12 @@ part1 = MIMEText(text, 'plain')
 part2 = MIMEText(html, 'html')
 
 msg.attach(part1)
-msg.attach(part2)
+# msg.attach(part2)
 
-s = smtplib.SMTP_SSL(smtp_server)
-s.login(from_address, smtp_password)
-s.sendmail("UIC Security Study <{0}>".format(from_address), to_address, msg.as_string().encode('utf-8').strip())
-s.quit()
+p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
+p.communicate(msg.as_string().encode('utf-8').strip())
+
+# s = smtplib.SMTP_SSL(smtp_server)
+# s.login(from_address, smtp_password)
+# s.sendmail("UIC Security Study <{0}>".format(from_address), to_address, msg.as_string().encode('utf-8').strip())
+# s.quit()
